64d09004b55c1087e64a702358938fa65b633804
karenaraujjo <karensilva443@gmail.com>
2025-06-03 21:40:34 -0300

    Aula1

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -1,1 +1,38 @@
+var idade
+const ano 
 //JavaScript
+console.log("Hello world") // log serve para apresentar uma mensagem 
+
+console.clear()   // clear vai limpar todas as info na tela
+
+//Variaveis
+var numero1 = 192 // Inicializar uma variavel - Signigica já dar um valor
+console.log(numero1)
+
+var num1 = 4
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 89 // Inicar a variável em outra parte do código
+console.log(idade)
+
+ano = 1936
+const PI = 3.14
+
+// Textos  - Strings (significa textos) =
+// Para inserir textos: var nome = "" ou var nome = ''
+var nome = 'Matheus Araujo'
+var nome = "Matheus Vinicius"
+var nome = '21032002' // Isto é texto, porque está dentro das aspas
+console.log(nome1)
+console.log(nome2)
+console.log(nome3)
+console.log(nome1, nome2, nome3) // Para inserir todos os nomes de uma vez, uiliza vírgula.
+
+//var = Flexível, menos regras (Posso alterar o valor inicial de uma var e recriar a mesma em outras partes do código)
+
+//let = não pode duplicar o nome, mas pode alterar o nome 
+// let = Você não pode recriar a mesma em outras partes do código com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+//const= Você não  pode recriar a mesma em outras partes  do codigo e não pode alterar o valor inicial dela garantindo a confiança do dado que esta sendo utilizadp no decorrer do seu código
