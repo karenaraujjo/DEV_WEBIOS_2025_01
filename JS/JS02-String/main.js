bb0dc746cf5fce4b1161e85eda530c6168c900f0
karenaraujjo <karensilva443@gmail.com>
2025-06-10 21:31:02 -0300

    Aula2Inicio

---
+++ JS/JS02-String/main.js
@@ -0,0 +1,35 @@
+// "A" - Caracter
+// "Olá" - Texto - String 
+
+var texto1 = "Olá IOS"
+//0 1 2 3 4 5 6 
+
+var resultado1= texto1.charAt(6) // Acessar caracter - 1º Opção
+console.log(resultado1)
+
+var resultado2 = texto1 [1] // Acessar caracter - 2º Opção
+console.log(resultado1)
+
+var lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+console.log(lorem.length)
+// console.log(lorem.length >= 245)
+
+var texto2 = "OLÁ TEC DE TECNOLOGIA"
+var texto3 = "olá, meu nome e ia, seu amigo"
+
+//Formato 1
+var resultado3 = texto2.toLowerCase()
+console.log(resultado3)
+//Formato 2
+console.log(texto3.toLocaleUpperCase())
+
+var texto4 = 'Mozilla'
+//  oz        0 1 2 3 4 5 6
+console.log(texto4.substring(1, 3))
+
+console.log(texto4.substring(1))
+
+var texto5 = 'Bem vindo a Microsoft'
+let novaMensagem = texto5.replace('Microsoft' , 'IOS')
+console.log(novaMensagem)
