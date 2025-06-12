b03922a35104872c9434320fc4028e0a4af10c35
karenaraujjo <karensilva443@gmail.com>
2025-06-12 20:52:19 -0300

    Aula4

---
+++ JS/JS04-Repeticao/main.js
@@ -0,0 +1,18 @@
+let contador = 0 // Inicialização
+// Enquanto a variavel contador for menor que 10, o while vai apresentar o valor da variavel no console
+//console.log(1)
+//console.log(2)
+
+while(contador < 10){
+    console.log(contador)
+    // contador = contador + 1 - Forma mais longa
+   // contador += 1  - Forma um pouco menos longa
+   contador ++ // Forma mais rápida
+}
+
+console.log('Finalizou o Programa')
+
+//  variavel inicial; condição, incremento
+for(contador = 0; contador <= 10; contador++){
+    console.log(contador)
+}
