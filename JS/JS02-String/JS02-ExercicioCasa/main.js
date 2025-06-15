3ae697cb576a3e452fcbcdf2ab8034937e673acc
Karen Araújo <karensilva443@gmail.com>
2025-06-14 21:27:32 -0300

    Exercicio.js

---
+++ JS/JS02-String/JS02-ExercicioCasa/main.js
@@ -0,0 +1,7 @@
+const minhaString = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log("Caractere na posição 19:", minhaString.charAt(19));
+
+console.log("Tamanho da string:", minhaString.length);
+
+console.log("String em maiúsculo:", minhaString.toUpperCase());
