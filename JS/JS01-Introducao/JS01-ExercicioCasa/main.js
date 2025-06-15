ec1bf9b3224ec14c29c9bdf82ad73a8ab8aa62e1
Karen Araújo <karensilva443@gmail.com>
2025-06-14 21:17:25 -0300

    Exercicio.js

---
+++ JS/JS01-Introducao/JS01-ExercicioCasa/main.js
@@ -0,0 +1,11 @@
+function calcularMedia(){
+    const nota1 = 8;
+    const nota2 = 7;
+    const nota3 = 7;
+    const nota4 = 9
+
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4;
+console.log(`A média do aluno com as 4 notas é: ${media}`)
+}
+calcularMedia();
